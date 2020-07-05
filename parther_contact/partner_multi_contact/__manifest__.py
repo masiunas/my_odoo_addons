64cf68a0271f88db91abee2caa6f0478ae1d5b80
masiunas <silencer368@gmail.com>
2020-07-05 16:49:00 +0300

    [ADD] Added Partner multi contact module with manifest.

---
+++ parther_contact/partner_multi_contact/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner multi contacts",
+    "version": "1.0",
+    "author": "Masiunas Euheni",
+    "license": "AGPL-3",
+    "category": "Contacts",
+    "summary": """
+    The module adds the ability to add any number of contacts (phone numbers, mailboxes, usernames etc.) for partners.
+    """,
+    "depends": ["base"],
+    "installable": True,
+}
